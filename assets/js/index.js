37871f349eb54dad2d197adb643136846ac689db
sipusumit <community.sumit@gmail.com>
2021-10-31 15:34:59 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-btn");
+const navMenu = document.getElementById("navMenu");
+const navContainer = document.getElementsByClassName("nav-container")[0];
+let menuOpen = false;
+menuBtn.addEventListener('click',()=>{
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        navMenu.classList.add("navMenuOpen");
+        navContainer.style.height = "300px"
+    }else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        navMenu.classList.remove("navMenuOpen");
+        navContainer.style.height = "80px"
+    }
+})
